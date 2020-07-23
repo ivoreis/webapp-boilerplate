007a592a0e215cb24f29e815a0bdd0808f0754f6
ivoreis <reis.ivo@gmail.com>
2020-07-23 13:48:37 +0100

    chore: add serverless example

--- next.config.js
+++ next.config.js
@@ -18,6 +18,17 @@ const webpackConfig = (config, options) => {
       use: [defaultLoaders.babel],
     })
   }
+
+  if (!process.env.BUNDLE_AWS_SDK) {
+    newConfig.externals = config.externals || [];
+    newConfig.externals.push({
+      "aws-sdk": "aws-sdk"
+    });
+  } else {
+    // eslint-disable-next-line
+    console.warn("Bundling aws-sdk. Only doing this in development mode");
+  }
+
   newConfig.resolve.alias.rosetta = dev ? 'rosetta/debug' : 'rosetta'
   newConfig.resolve.alias['~'] = path.resolve(__dirname, './src')
   return newConfig
---
+++ serverless.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'webapp-boilerplate': {
+    component: "@sls-next/serverless-component@1.15.1"
+  }
+}
